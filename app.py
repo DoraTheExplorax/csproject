fa6ffa104ebb1a0399525293006b013cd6872627
shim1998 <98shim@gmail.com>
2021-09-09 00:42:40 +0530

    hyperlinking works

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+   return render_template('login.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+   return render_template('signup.html')
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
